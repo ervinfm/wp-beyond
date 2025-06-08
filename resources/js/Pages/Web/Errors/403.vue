e561d44e1aa36d5e3c4232913d2ca66e2bb570a8
Ervin Fikot M <ervinfm14@gmail.com>
2025-06-08 16:32:33 +0700

    update 8 juni 2025

--- resources/js/Pages/Web/Errors/403.vue
+++ resources/js/Pages/Web/Errors/403.vue
@@ -10,13 +10,13 @@ defineProps({ status: Number });
     <Head title="403 - Akses Ditolak" />
     <main class="flex items-center justify-center bg-white py-24 sm:py-32">
         <div class="text-center">
-            <p class="text-base font-semibold text-orange-600">{{ status }}</p>
+            <p class="text-base font-semibold text-yellow-600">{{ status }}</p>
             <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Akses Ditolak</h1>
             <p class="mt-6 text-base leading-7 text-gray-600">
                 Maaf, Anda tidak memiliki izin untuk mengakses halaman ini.
             </p>
             <div class="mt-10">
-                <Link href="/" class="rounded-md bg-orange-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-orange-500">
+                <Link href="/" class="rounded-md bg-yellow-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500">
                     Kembali ke Beranda
                 </Link>
             </div>
--- resources/js/Pages/Web/Errors/404.vue
+++ resources/js/Pages/Web/Errors/404.vue
@@ -16,7 +16,7 @@ defineProps({
 
     <main class="flex items-center justify-center bg-white py-24 sm:py-32">
         <div class="text-center">
-            <p class="text-base font-semibold text-blue-600">{{ status }}</p>
+            <p class="text-base font-semibold text-yellow-600">{{ status }}</p>
             <h1 class="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">Halaman Tidak Ditemukan</h1>
             <p class="mt-6 text-base leading-7 text-gray-600">
                 Maaf, kami tidak dapat menemukan halaman yang Anda cari. Mungkin telah dipindahkan atau dihapus.
@@ -24,7 +24,7 @@ defineProps({
             <div class="mt-10 flex items-center justify-center gap-x-6">
                 <Link 
                     href="/" 
-                    class="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                    class="rounded-md bg-yellow-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-yellow-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-yellow-600"
                 >
                     Kembali ke Beranda
                 </Link>
